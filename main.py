26dd155f262c60b0456fa1828c9618675da34465
Abhishek Gawade <147148460+abhigawade@users.noreply.github.com>
2026-01-31 20:43:05 +0530

    Added sort list without using sort function

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # Python Practice Questions for Logic Building
+
+# Sort List without using sort function
+li = [2,6,1,5,8]
+for i in range(0, len(li)):
+    for j in range(i+1, len(li)):
+        if li[i] > li[j]:
+            li[i], li[j] = li[j], li[i]
+print(li)
