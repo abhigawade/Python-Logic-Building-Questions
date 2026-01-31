4b4fdd4462185cfc935f1a3dc245120181853a00
Abhishek Gawade <147148460+abhigawade@users.noreply.github.com>
2026-01-31 20:52:04 +0530

    Implement counting of list items in a dictionary

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ for i in range(0, len(li)):
         if li[i] > li[j]:
             li[i], li[j] = li[j], li[i]
 print(li)
+
+# Add list items into dictionary as key and its count as value
+li = [1,3,5,2,1,5,6,3,3]
+new_dict = {}
+for i in li:
+    new_dict[i] = li.count(i)
+    
+print(new_dict)
