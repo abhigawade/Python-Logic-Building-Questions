3e3ee88c3b419c04369733d06dd8a4fa97469d45
Abhishek Gawade <147148460+abhigawade@users.noreply.github.com>
2026-02-02 16:23:17 +0530

    Added String questions for logic building

---
+++ string_questions.py
@@ -0,0 +1,21 @@
+# String Questions
+# Check String is Palindrome or not
+str_one = 'nitin'
+rev_str = str_one[::-1]
+if str_one == rev_str:
+    print('String is Palindrome')
+else:
+    print('String is not Palindrome')
+
+
+# Find a Length of string
+# First Method
+st = 'hello_world'
+print(len(st))
+
+# Second Method
+count = 0
+for i in st:
+    count += 1
+print(count)
+
