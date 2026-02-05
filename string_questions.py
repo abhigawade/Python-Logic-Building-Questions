1c392e72b2704a31a8764e4d412d099b7a1e5097
Abhishek Gawade <147148460+abhigawade@users.noreply.github.com>
2026-02-05 22:53:13 +0530

    added some string questions

--- string_questions.py
+++ string_questions.py
@@ -67,3 +67,32 @@ st_one = 'helloworlduser'
 length = len(st_one)//2
 st_two = st_one[:length].upper() + st_one[length:]
 print(st_two)
+
+# Make first letter of every word of string capital
+st_one = 'my name is a abhi'
+li_one = st_one.split()
+print(li_one)
+
+li_two = []
+for i in li_one:
+    if len(i) == 1:
+        new_i = i.upper()
+    else:
+        new_i = i[0].upper() + i[1:-1] + i[-1].upper()
+    li_two.append(new_i)
+    
+new_str = ' '.join(li_two)
+print(new_str)
+
+#check if a string has at least one letter and one number
+st_one = 'my name is a abhi age 23 '
+
+for i in st_one:
+    flag = True if i.isalpha() or i.isdigit() else False
+    if flag:
+        break
+
+if flag:
+    print('Yes it has')
+else:
+    print('No')
